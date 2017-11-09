62af3561cb94b36c6b6f13801ddef43396ceaba4
AFilipov2017 <phillipov@profline-opt.ru>
2017-11-09 19:31:40 +0200

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,20 @@
+/**
+* @author afilippov
+* @version 1
+* @since 09.11.2017
+*/
 package ru.job4j;
+
 
 public class Calculate{
 
 public static void main(String[] args){
 	System.out.println("Hello world");
 	}
+	/**
+	 * Сложение
+	 * @param first первый аргумент.
+	 * @param second второй аргумент
+	 * @return результат
+	 */
 }
