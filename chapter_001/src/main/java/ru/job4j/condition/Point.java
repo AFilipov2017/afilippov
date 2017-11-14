5bfff174e834dc83c99f1e3b50e175ba69ff5f51
AFilipov2017 <phillipov@profline-opt.ru>
2017-11-14 22:36:31 +0200

    3.2. Положение точки [#188]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,35 @@
+package ru.job4j.condition;
+/**
+ * @author afilippov
+ * @version 1
+ * @since 14.11.2017
+ */
+ 
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public boolean is(int a, int b) {
+
+
+        if (y == a * x + b) {
+
+            return true;
+        }
+        return false;
+    }
+}
+
---
+++ chapter_001/src/test/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for point task.
+*
+* @author Andrey Filippov (afilipov1980@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.condition;
