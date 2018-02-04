dbf15cf91e0cd1d2b4a2719e232bdab5bf2c6349
AFilipov2017 <phillipov@profline-opt.ru>
2018-02-04 15:08:37 +0200

    1. Реализация профессий в коде [#6837][#39687]

---
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -0,0 +1,30 @@
+package ru.job4j.profession;
+
+public class Profession {
+   public String name;
+   public String tools;
+
+   public Profession() {
+
+   }
+
+   public Profession(String name, String tools) {
+       this.name = name;
+       this.tools = tools;
+    }
+
+    public Profession(String name) {
+
+       this.name = name;
+    }
+
+    public String getName() {
+
+       return name;
+    }
+
+    public String getTools() {
+
+       return tools;
+    }
+}
