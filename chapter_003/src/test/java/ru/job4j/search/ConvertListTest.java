9a9a19372147365f83f34bd690aced409a2a3ded
AFilipov2017 <phillipov@profline-opt.ru>
2018-03-13 17:44:11 +0200

    Конвертация двумерного массива в ArrayList и наоборот [#10035]

---
+++ chapter_003/src/test/java/ru/job4j/search/ConvertListTest.java
@@ -0,0 +1,77 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 1
+ * @since
+ */
+public class ConvertListTest {
+    @Test
+    public void whenArrayConvertToList() {
+        ConvertList convertList = new ConvertList();
+        int[][] a = new int[][]{{1, 2}, {3, 4}};
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        List<Integer> result = convertList.toList(a);
+        assertThat(result, is(list));
+    }
+
+    @Test
+    public void whenArrayConvertToListTwo() {
+        ConvertList convertList = new ConvertList();
+        int[][] a = new int[][]{{1, 2}, {3, 4}, {5, 6}};
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        List<Integer> result = convertList.toList(a);
+        assertThat(result, is(list));
+    }
+
+    @Test
+    public void whenListConvertToArray() {
+        ConvertList convertList = new ConvertList();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        int[][] expect = {{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
+        int[][] result = convertList.toArray(list, 3);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenListConvertToArrayTwo() {
+        ConvertList convertList = new ConvertList();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        int[][] expect = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+        int[][] result = convertList.toArray(list, 2);
+        assertThat(result, is(expect));
+    }
+}
