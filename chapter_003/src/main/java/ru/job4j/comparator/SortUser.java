e46f2dc2623cae10d22dbd875c4e2191de21733b
AFilipov2017 <phillipov@profline-opt.ru>
2018-03-15 18:08:43 +0200

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/comparator/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/comparator/SortUser.java
@@ -10,12 +10,45 @@ import java.util.*;
 public class SortUser {
     /**
      * Метод возвращает TreeSet пользователей, отсортированных по возрасту в порядке возрастания.
+     *
      * @param list список имен и возрастов.
      */
     public Set<User> sort(List<User> list) {
         Set<User> user = new TreeSet<>();
         user.addAll(list);
         return user;
+    }
+
+    /**
+     * Метод сортирует пользователей по длине имени.
+     *
+     * @param list список имен и возрастов.
+     */
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getName().length(), (o2.getName().length()));
+            }
+        });
+        return null;
+    }
+
+    /**
+     * Метод сортирует пользователей по длине имени и по возрасту.
+     *
+     * @param list список имен и возрастов.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result = Integer.compare(o1.getName().length(), (o2.getName().length()));
+
+                return result != 0 ? result : Integer.compare(o1.getAge(), o2.getAge());
+            }
+        });
+        return null;
     }
 }
 
--- chapter_003/src/test/java/ru/job4j/comparator/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/comparator/SortUserTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.comparator;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,7 +13,7 @@ import java.util.Set;
 /**
  * @author Andrey Filippov (afilipov1980@gmail.com)
  * @version 1
- * @since
+ * @since 15.03.2018
  */
 public class SortUserTest {
     @Test
@@ -25,6 +26,7 @@ public class SortUserTest {
         int result = data.get(0).getAge();
         assertThat(result, is(25));
     }
+
     @Test
     public void whenListSortedToSet2() {
         SortUser user = new SortUser();
@@ -34,5 +36,47 @@ public class SortUserTest {
         data.addAll(sort);
         int result = data.get(2).getAge();
         assertThat(result, is(82));
+    }
+
+    @Test
+    public void whenListSortedByNameLength() {
+        SortUser user = new SortUser();
+        List<User> list = new ArrayList<>(Arrays.asList(new User("Andrey", 25), new User("Alex", 30), new User("Veniamin", 33)));
+        user.sortNameLength(list);
+        String result = list.get(0).getName();
+        assertThat(result, is("Alex"));
+    }
+
+    @Test
+    public void whenListSortedByAllFields() {
+        List<User> list = new ArrayList<>(Arrays.asList(new User("Andrey", 25), new User("Alex", 30), new User("Veniamin", 33)));
+        List<User> list1 = new ArrayList<>(Arrays.asList(new User("Andrey", 30), new User("Alex", 31), new User("Veniamin", 29)));
+        list.addAll(list1);
+        SortUser sort = new SortUser();
+        sort.sortByAllFields(list);
+        int result = list.get(0).getAge();
+        assertThat(result, is(30));
+    }
+
+    @Test
+    public void whenListSortedByAllFieldsTwo() {
+        List<User> list = new ArrayList<>(Arrays.asList(new User("Andrey", 25), new User("Alex", 30), new User("Veniamin", 33)));
+        List<User> list1 = new ArrayList<>(Arrays.asList(new User("Andrey", 30), new User("Alex", 31), new User("Veniamin", 29)));
+        list.addAll(list1);
+        SortUser sort = new SortUser();
+        sort.sortByAllFields(list);
+        int result = list.get(5).getAge();
+        assertThat(result, is(33));
+    }
+
+    @Test
+    public void whenListSortedByAllFieldsThree() {
+        List<User> list = new ArrayList<>(Arrays.asList(new User("Andrey", 25), new User("Alex", 30), new User("Veniamin", 33)));
+        List<User> list1 = new ArrayList<>(Arrays.asList(new User("Andrey", 30), new User("Alex", 31), new User("Veniamin", 29)));
+        list.addAll(list1);
+        SortUser sort = new SortUser();
+        sort.sortByAllFields(list);
+        String result = list.get(5).getName();
+        assertThat(result, is("Veniamin"));
     }
 }
