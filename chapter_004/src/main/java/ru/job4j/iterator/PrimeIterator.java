ea01fef81ed9c85cdb1a6eb76b2a7b0cf37ce887
AFilipov2017 <phillipov@profline-opt.ru>
2018-04-08 15:22:10 +0200

    5.1.3. Создать итератор простые числа. [#151]

---
+++ chapter_004/src/main/java/ru/job4j/iterator/PrimeIterator.java
@@ -0,0 +1,51 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 1
+ * @since 8.04.2018
+ */
+public class PrimeIterator implements Iterator<Integer> {
+    private final int[] values;
+    private int index = 0;
+
+    public PrimeIterator(int[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean res = false;
+        for (int i = index; i < values.length; i++) {
+            res = isPrime(values[i]);
+        }
+        return res;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (!isPrime(values[index])) {
+            index++;
+        }
+        return values[index++];
+    }
+
+    public static boolean isPrime(int num) {
+        boolean result = true;
+        if (num < 2) {
+            result = false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
