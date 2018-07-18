b642c9063080e752dc12bf21d0a76ba848574372
AFilipov2017 <phillipov@profline-opt.ru>
2018-07-18 13:52:43 +0300

    Крестики-нолики на JavaFX [#49777]

---
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -0,0 +1,64 @@
+package ru.job4j.tictactoe;
+
+import java.util.function.Predicate;
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 15.07.2018
+ */
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean isWinnerX() {
+         return  this.fillBy(Figure3T::hasMarkX, 0 , 0, 1, 0)
+                 || this.fillBy(Figure3T::hasMarkX, 0 , 1, 1, 0)
+                 || this.fillBy(Figure3T::hasMarkX, 0 , 2, 1, 0)
+                 || this.fillBy(Figure3T::hasMarkX, 0 , 0, 0, 1)
+                 || this.fillBy(Figure3T::hasMarkX, 1 , 0, 0, 1)
+                 || this.fillBy(Figure3T::hasMarkX, 2 , 0, 0, 1)
+                 || this.fillBy(Figure3T::hasMarkX, 0 , 2, 1, -1)
+                 || this.fillBy(Figure3T::hasMarkX, 0 , 0, 1, 1);
+    }
+
+    public boolean isWinnerO() {
+        return  this.fillBy(Figure3T::hasMarkX, 0 , 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0 , 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0 , 2, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0 , 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 1 , 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 2 , 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0 , 2, 1, -1)
+                || this.fillBy(Figure3T::hasMarkX, 0 , 0, 1, 1);
+    }
+
+    public boolean hasGap() {
+        boolean result = false;
+        for (int i = 0; i < table.length ; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+            if (!table[i][j].hasMarkX() && !table[i][j].hasMarkO()) {
+                result = true;
+            }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.tictactoe;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 15.07.2018
+ */
+
+public class Logic3TTest {
+
+    @Test
+    public void whenHasXWinner() {
+        Figure3T[][] table = {
+                {new Figure3T(true), new Figure3T(), new Figure3T()},
+                {new Figure3T(), new Figure3T(true), new Figure3T()},
+                {new Figure3T(), new Figure3T(), new Figure3T(true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.isWinnerX(), is(true));
+    }
+
+    @Test
+    public void whenHasOWinner() {
+        Figure3T[][] table = {
+                {new Figure3T(true), new Figure3T(), new Figure3T()},
+                {new Figure3T(false), new Figure3T(true), new Figure3T()},
+                {new Figure3T(false), new Figure3T(), new Figure3T(true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.isWinnerO(), is(true));
+    }
+
+    @Test
+    public void whenHasGas() {
+        Figure3T[][] table = {
+                {new Figure3T(true), new Figure3T(), new Figure3T()},
+                {new Figure3T(), new Figure3T(true), new Figure3T()},
+                {new Figure3T(), new Figure3T(), new Figure3T(true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.hasGap(), is(true));
+    }
+}
