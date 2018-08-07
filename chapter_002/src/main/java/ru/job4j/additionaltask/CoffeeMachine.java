e4e415049bbccfdf29dc8c82cb833fe494cf39f7
AFilipov2017 <phillipov@profline-opt.ru>
2018-08-07 19:44:46 +0300

    дополнительное задание Кофемашина. [#34741]

---
+++ chapter_002/src/main/java/ru/job4j/additionaltask/CoffeeMachine.java
@@ -0,0 +1,41 @@
+package ru.job4j.additionaltask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 07.08.2018
+ */
+public class CoffeeMachine {
+    public Integer[] changes(int value, int price) {
+        int[] coin = {10, 5, 2, 1};
+        List<Integer> list = new ArrayList<>();
+        Integer[] result;
+        int a = value - price;
+        int index = 0;
+
+        while (a != 0) {
+            if (a >= coin[index]) {
+                list.add(coin[index]);
+                a -= coin[index];
+            } else {
+                ++index;
+            }
+        }
+        result = new Integer[list.size()];
+        result = list.toArray(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        Integer[] result = coffeeMachine.changes(73, 30);
+        for (Integer i : result
+                ) {
+            System.out.println(i);
+        }
+    }
+}
+
