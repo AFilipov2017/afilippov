d7b17b910b64b79951e9a8e8c922c25f1aca9f9a
AFilipov2017 <phillipov@profline-opt.ru>
2018-09-12 18:52:50 +0300

    Доп задание метод сравнивающий две строки за O(n)

---
+++ chapter_004/src/main/java/ru/job4j/additionaltask/EqualString.java
@@ -0,0 +1,30 @@
+package ru.job4j.additionaltask;
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 12.09.2018
+ */
+public class EqualString {
+
+    public boolean equalsString(String o, String t) {
+        int size = Integer.min(o.length(), t.length());
+        boolean result = true;
+        if (o == t) {
+            return true;
+        }
+        if (o.length() != t.length()) {
+            result = false;
+        } else {
+            char[] one = o.toCharArray();
+            char[] two = t.toCharArray();
+
+            for (int i = 0; i < size; i++) {
+                if (one[i] != two[i]) {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/additionaltask/EqualStringTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.additionaltask;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 12.09.2018
+ */
+public class EqualStringTest {
+
+    @Test
+    public void whenToCompareTwoDifferentStrings() {
+        EqualString eqs = new EqualString();
+        String o = "mama";
+        String t = "amam";
+        boolean result = eqs.equalsString(o, t);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenToCompareTwoSameStrings() {
+        EqualString eqs = new EqualString();
+        String o = "mama";
+        String t = "mama";
+        boolean result = eqs.equalsString(o, t);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenToCompareTwoDifferentLengthStrings() {
+        EqualString eqs = new EqualString();
+        String o = "maman";
+        String t = "mama";
+        boolean result = eqs.equalsString(o, t);
+        assertThat(result, is(false));
+    }
+}
