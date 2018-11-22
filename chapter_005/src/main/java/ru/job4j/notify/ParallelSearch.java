d6b3154d8bbd9b1f0028aa03fbdfd6d7de56726d
AFilipov2017 <phillipov@profline-opt.ru>
2018-11-22 13:54:07 +0200

    2. Обеспечить остановку потребителя. [#66825]

---
+++ chapter_005/src/main/java/ru/job4j/notify/ParallelSearch.java
@@ -0,0 +1,41 @@
+package ru.job4j.notify;
+
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 22.11.2018
+ */
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                            Thread.sleep(501);
+                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
