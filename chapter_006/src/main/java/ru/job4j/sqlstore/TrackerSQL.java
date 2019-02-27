2f24353b853f15677572bad271d97e3ebdc2d58a
AFilipov2017 <phillipov@profline-opt.ru>
2019-02-27 19:51:45 +0200

    4.2. Трекер SQL [#1734]

---
+++ chapter_006/src/main/java/ru/job4j/sqlstore/TrackerSQL.java
@@ -0,0 +1,212 @@
+package ru.job4j.sqlstore;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.job4j.tracker.models.Item;
+import ru.job4j.tracker.start.ITracker;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 20.02.2019
+ */
+public class TrackerSQL implements ITracker, AutoCloseable {
+    private static final Logger LOG = LogManager.getLogger(TrackerSQL.class.getName());
+    private Connection connection;
+
+    public boolean init() {
+        try (InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            this.connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return this.connection != null;
+    }
+
+    public boolean checkTableExist() {
+        init();
+        boolean result = false;
+        try {
+            DatabaseMetaData metadata = connection.getMetaData();
+            ResultSet resultSet = metadata.getTables(null, null, "tracker", null);
+            if (resultSet.next()) {
+                return true;
+            } else {
+                PreparedStatement st = connection.prepareStatement("CREATE TABLE  tracker(id serial primary key, item_id varchar(50) , name varchar (30), description varchar (200), created_date timestamp, comment text );");
+                st.executeUpdate();
+                st.close();
+                result = true;
+            }
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return result;
+    }
+
+    @Override
+    public Item add(Item item) {
+        init();
+        try (PreparedStatement st = connection.prepareStatement("INSERT INTO tracker(item_id, name, description, created_date, comment) values (?, ?, ?, ?, ?);")) {
+            st.setString(1, item.getId());
+            st.setString(2, item.getName());
+            st.setString(3, item.getDescription());
+            st.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            st.setString(5, item.getComments());
+            st.executeUpdate();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+        return item;
+    }
+
+    @Override
+    public void replace(String id, Item item) {
+        init();
+        try (PreparedStatement st = connection.prepareStatement("UPDATE tracker SET item_id = ?, name = ?, description = ?, created_date = ?, comment = ? WHERE id = ?;")) {
+            st.setString(1, item.getId());
+            st.setString(2, item.getName());
+            st.setString(3, item.getDescription());
+            st.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            st.setString(5, item.getComments());
+            st.setInt(6, Integer.parseInt(id));
+            st.executeUpdate();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void delete(String id) {
+        init();
+        try (PreparedStatement st = connection.prepareStatement("DELETE FROM tracker WHERE id = ?;")) {
+            st.setInt(1, Integer.parseInt(id));
+            st.executeUpdate();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public List<Item> findAll() {
+        init();
+        List<Item> list = new ArrayList<>();
+        try (PreparedStatement st = connection.prepareStatement("SELECT name, description, created_date, comment FROM tracker;")) {
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Timestamp ts = rs.getTimestamp("created_date");
+                long date = ts.getTime();
+                list.add(new Item(rs.getString("name"), rs.getString("description"), date, rs.getString("comment")));
+            }
+            rs.close();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        init();
+        List<Item> list = new ArrayList<>();
+        try (PreparedStatement st = connection.prepareStatement("SELECT name, description, created_date, comment FROM tracker WHERE name like ?;")) {
+            st.setString(1, key);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Timestamp ts = rs.getTimestamp("created_date");
+                long date = ts.getTime();
+                list.add(new Item(rs.getString("name"), rs.getString("description"), date, rs.getString("comment")));
+            }
+            rs.close();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public Item findById(String id) {
+        init();
+        Item item = new Item();
+        try (PreparedStatement st = connection.prepareStatement("SELECT name, description, created_date, comment FROM tracker WHERE id = ?;")) {
+            st.setInt(1, Integer.parseInt(id));
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Timestamp ts = rs.getTimestamp("created_date");
+                long date = ts.getTime();
+                item = new Item(rs.getString("name"), rs.getString("description"), date, rs.getString("comment"));
+            }
+            rs.close();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+        return item;
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.connection.close();
+    }
+}
---
+++ chapter_006/src/test/java/sqlstore/TrackerSQLTest.java
@@ -0,0 +1,83 @@
+package sqlstore;
+
+import org.junit.Test;
+import ru.job4j.sqlstore.TrackerSQL;
+import ru.job4j.tracker.models.Item;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Andrey Filippov (afilipov1980@gmail.com)
+ * @version 0.1
+ * @since 26.02.2019
+ */
+public class TrackerSQLTest {
+    private static final Random RAND = new Random();
+
+    private String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RAND.nextInt());
+    }
+
+    @Test
+    public void checkConnection() {
+        TrackerSQL sql = new TrackerSQL();
+        assertThat(sql.init(), is(true));
+    }
+
+    @Test
+    public void checkTable() {
+        TrackerSQL sql = new TrackerSQL();
+        assertThat(sql.checkTableExist(), is(true));
+    }
+
+    @Test
+    public void whenAddItem() {
+        Date today = new Date();
+        Item item = new Item("test", "testDescription", today.getTime(), "comm");
+        Item item1 = new Item("test1", "testDescription1", today.getTime(), "comm1");
+        Item item2 = new Item("test2", "testDescription2", today.getTime(), "comm2");
+        item.setId(this.generateId());
+        item1.setId(this.generateId());
+        item2.setId(this.generateId());
+        TrackerSQL sql = new TrackerSQL();
+        sql.checkTableExist();
+        sql.add(item);
+        sql.add(item1);
+        sql.add(item2);
+        List<Item> list = sql.findAll();
+        assertThat(list.get(0).getName(), is("test"));
+        assertThat(list.get(1).getName(), is("test1"));
+        assertThat(list.get(2).getName(), is("test2"));
+
+//        whenFindItemByName
+        List<Item> list1 = sql.findByName("test1");
+        assertThat(list1.get(0).getComments(), is("comm1"));
+
+//        whenFindItemById
+        Item it = sql.findById("2");
+        assertThat(it.getName(), is("test1"));
+
+//        whenReplaceItem
+        Item item3 = new Item("test3", "testDescription3", today.getTime(), "comm3");
+        item3.setId(this.generateId());
+        sql.replace("2", item3);
+        List<Item> list2 = sql.findAll();
+        assertThat(list2.get(2).getName(), is("test3"));
+
+//        whenDeleteItem
+        sql.delete("2");
+        List<Item> list3 = sql.findAll();
+        assertThat(list3.size(), is(2));
+    }
+}
+
+
+
+
+
+
